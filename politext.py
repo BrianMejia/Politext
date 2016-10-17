4183363aaabca0cde07532a8c36b883ba4248ccd
BrianMejia <BMejia0727@gmail.com>
2016-10-17 15:16:52 -0400

    web running

--- politext.py
+++ politext.py
@@ -44,7 +44,7 @@ favorable_rating_sectors = {"stein": "jill-stein", "johnson": "gary-johnson", "k
 @app.route("/sms", methods=['GET', 'POST'])
 def get_app_msg():
 	body = request.values.get('Body', None)
-    resp = twilio.twiml.Response()
+	resp = twilio.twiml.Response()
 	resp.message(interpret_message(body))
 	return str(resp)
 
