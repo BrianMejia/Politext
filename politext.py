1ec2e799b92e1f7f7d3757dc9245ca00d67a248a
BrianMejia <BMejia0727@gmail.com>
2016-10-18 08:20:43 -0400

    Checking for phone numbers

--- politext.py
+++ politext.py
@@ -44,8 +44,9 @@ favorable_rating_sectors = {"stein": "jill-stein", "johnson": "gary-johnson", "k
 @app.route("/sms", methods=['GET', 'POST'])
 def get_app_msg():
 	body = request.values.get('Body', None)
+	number = request.values.get('From', None)
 	resp = twilio.twiml.Response()
-	resp.message(interpret_message(body))
+	resp.message('#', str(number), ' ', interpret_message(body))
 	return str(resp)
 
 def interpret_message(msg):
